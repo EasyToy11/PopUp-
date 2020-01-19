64a9d3b94905855bb090b00f7a76820343485230
kara_box <50469502+EasyToy11@users.noreply.github.com>
2020-01-19 23:07:01 +0900

    Add files via upload

--- class_frame.py
+++ class_frame.py
@@ -23,33 +23,40 @@ class Application(tk.Frame):
         label2 = tk.Label(text="テスト")
         label2.pack()
 
-        entry1 = tk.Entry(width=40)
+        self.entry1 = tk.Entry(width=40)
+        self.entry1.pack()
+
+        button1 = tk.Button(text="保存",  command=self.save_comment)
+        button1.pack()
 
     def update(self):
         label3 = tk.Label(text=datetime.now().strftime("%Y/%m/%d %H:%M:%S"))
-        label3.place(x=40, y=175)
+        label3.place(width=100, height=300)
 
         # .after(delay, callback=None)
         # は、すべてのtkinterウィジェットに対して定義されたメソッドです。
         self.master.after(50, self.update)
 
     def save_comment(self):
-        source_path = r"C:\Users\n1270242.STCN2\Downloads\timetable"
+        source_path = r"C:\Users\kurra\Downloads"
         is_day = [0] * 2
+        # time get
         a = datetime.now()
         file_index = a.day
         dir_index = a.month
-        txt = entry1.get()
+        # content get
+        txt = self.entry1.get()
         self.entry1.delete(0, tk.END)
 
+        # each path setting
         dir_path = source_path + '\\' + str(dir_index) + '月'
         txt_path = dir_path + '\\' + str(file_index) + '日.txt'
-        # 月ごとにディレクトリーを作る
-        if is_day[0] != dir_index and (os.path.isdir(dir_path) is False):
+        # 月ごとにディレクトリーを作る。
+        if os.path.isdir(dir_path) is False:
             os.mkdir(dir_path)
 
         # その日なら追記
-        if is_day[1] == file_index or (os.path.isfile(txt_path) is True):
+        if os.path.isfile(txt_path) is True:
             with open(txt_path, mode='a') as f:
                 f.write(f'\n{a.hour}:{a.minute}')
                 f.write('\n' + txt + '\n')
@@ -61,7 +68,6 @@ class Application(tk.Frame):
 
         is_day[0] = dir_index
         is_day[1] = file_index
-
 
 
 def main():
